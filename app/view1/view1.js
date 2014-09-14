874e32ace88f8fe55c24e8681d8355704f25f41f
katya sanko <katya.sanko@gmail.com>
2014-09-14 22:32:31 +0300

    dictionary complited

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -15,18 +15,30 @@ angular.module('myApp.view1', ['ngRoute'])
 
 	$scope.performAction = function () {
 		//debugger;
-		if(($scope.keyword != null) || ($scope.keyword != undefined) || ($scope.key != null) || ($scope.key != undefined) || (parseInt($scope.key) < 26)) {
+		if(($scope.keyword != null) || ($scope.keyword != undefined) || ($scope.key != null) || ($scope.key != undefined) || (parseInt($scope.key) < 25)) {
 			var dictionary = {'a':'',  'b':'', 'c':'', 'd':'', 'e':'', 'f':'', 'g':'', 'h':'', 'i':'', 'j':'', 'k':'', 'l':'', 'm':'', 'n':'', 'o':'', 'p':'', 'q':'', 'r':'', 's':'', 't':'', 'u':'', 'v':'', 'w':'', 'x':'', 'y':'', 'z':''
-			}, startPosition = parseInt($scope.key), keywordLength = $scope.keyword.length;
+			}, startPosition = parseInt($scope.key), keywordLength = $scope.keyword.length, currentPosition = startPosition;
 			console.log('fdf');
 			// to do properties from array
 			var array = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 			for (var i = 0;  i < keywordLength; i++) {
-				alert('hello');
-				dictionary[Object.keys(dictionary)[startPosition]] = $scope.keyword[i];
-				array.splice(array.indexOf($scope.keyword), 1);
-				console.log(array);
+				if (currentPosition > 25) {
+					currentPosition = 0;
+				}
+				console.log('hello' + $scope.keyword[i]);
+				dictionary[Object.keys(dictionary)[currentPosition]] = $scope.keyword[i];
+				array.splice(array.indexOf($scope.keyword[i]), 1);
+				currentPosition++;
 			}
+			for (var i = 0;  i < array.length; i++) {
+				if (currentPosition > 25) {
+					currentPosition = 0;
+				}
+				dictionary[Object.keys(dictionary)[currentPosition]] = array[i];
+				currentPosition++;
+			}
+			console.log(array);
+			console.log(dictionary);
 
 		}
 
